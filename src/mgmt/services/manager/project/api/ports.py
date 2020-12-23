d2f5ba3e7c94e7b1154581d2eb840372fe746dc1
Rio Zhu <zzhu@futurewei.com>
2020-12-23 18:15:13 +0000

    Removed logs

--- src/mgmt/services/manager/project/api/ports.py
+++ src/mgmt/services/manager/project/api/ports.py
@@ -33,7 +33,6 @@ def all_ports():
         amount_of_ports = len(portList)
         logger.debug(f'Start to make {amount_of_ports} ports.')
         start_time = time.time()
-        # need to add dicts, not Port/Host
         ports_to_add = []
         eps_to_add = []
         hosts_to_add = []
@@ -48,44 +47,28 @@ def all_ports():
             }
 
             ip_node_to_find = post_data.get('ip_node')
-            logger.debug(f'Need to find ip_node: [{ip_node_to_find}]')
             host = Host.query.filter_by(ip_node=ip_node_to_find).first()
             if (host is None) and (ip_node_to_find not in host_ip_node_lookup_set):
-                logger.debug(f'Cannot find ip_node [{ip_node_to_find}] in either the db or the hosts to add, create a new host to add')
                 host_to_add = {'mac_node': post_data.get('mac_node'),
                     'ip_node' : ip_node_to_find,
                     'host_id' : str(uuid.uuid4())}
                 port['host_id'] = host_to_add['host_id']
-                logger.debug(f'Created this host_to_add: {host_to_add}')
                 hosts_to_add.append(host_to_add)
                 host_ip_node_lookup_set.add(ip_node_to_find)
             elif (host is not None):
-                logger.debug(f'In DB, found host with host_id: [{host.host_id}]')
                 port['host_id'] = host.host_id
             elif (ip_node_to_find in host_ip_node_lookup_set):
-                logger.debug(f'Host_id: [{ip_node_to_find}] should be found in hosts_to_add')
                 for host_to_add in hosts_to_add:
                     if host_to_add['ip_node'] == ip_node_to_find:
-                        logger.debug(f'In hosts_to_add, found host with host_id: [{ip_node_to_find}]')
                         port['host_id'] = host_to_add['host_id']
                         break
-                logger.debug(f'Host_id: [{ip_node_to_find}] should be found in hosts_to_add, but it is not found after the while loop, need to investigate')
-            else:
-                logger.debug(f'Something weird happend for querying host and looking up in the hosts_to_add')
-                logger.debug(f'host in query: {host}')
-                logger.debug(f'hosts_to_add: ')
-                for host_to_add in hosts_to_add:
-                    logger.debug(f'One host_to_add: {host_to_add}')
             for ip in post_data['ips_port']:
                 ep_to_add = {
                                 'ip': ip['ip'],
                                 'vip': ip['vip'],
                                 'port_id': post_data['port_id']
                             }
-                # port['eps'].append(ep_to_add)
                 eps_to_add.append(ep_to_add)
-                logger.debug(f'created this ep: {ep_to_add}')
-            logger.debug(f'Adding this port object to the list: \n{port}')
             ports_to_add.append(port)
         db.session.bulk_insert_mappings(Host, hosts_to_add)
         db.session.commit()
